8421f37381482101b9661720407876a1f7fe3c19
Paji1 <tahamaroc1@gmail.com>
2023-07-14 00:21:09 +0100

    feat: clean junk files

--- len.js
+++
@@ -1,7 +0,0 @@
-
-const fs = require('fs');
-
-// Read the JSON data from the file
-const jsonData = fs.readFileSync('ex.json', 'utf8');
-const cursusData = JSON.parse(jsonData);
-console.log(cursusData.length);
