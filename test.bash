3aae6a2d84db43c9a95b0b838d3290829c117fc2
Paji1 <tahamaroc1@gmail.com>
2023-08-15 19:30:27 +0100

    feat: refactor

--- test.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Read and process each object in userData.json
-jq -c '.[]' usersData.json | while IFS= read -r userObject; do
-    # Send the POST request using curl
-  curl -X POST -H "Content-Type: application/json" -d '{"user":'"$userObject"'}' http://localhost/add
-done
