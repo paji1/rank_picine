d086242875e732f46934158bb2c9b7bcb54bfbb4
Taha El mouhajir <47013396+paji1@users.noreply.github.com>
2023-09-11 06:57:29 +0000

    fix: change to cookie-session

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('express-session');
+const session = require('cookie-session');
 const cors = require('cors');
 const routes = require('./routes'); // Import your routes module
 require('dotenv').config();
